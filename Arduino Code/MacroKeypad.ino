f7a0c74a94ad53bb0b1a4fbb69b875a75f1bbf1c
Preston Crowe <prestoncrowe2004@gmail.com>
2019-02-15 22:13:01 -0800

    Add files via upload

---
+++ Arduino Code/MacroKeypad.ino
@@ -0,0 +1,105 @@
+// Original Code made by Foster Phillips
+// Original Instructable: https://www.instructables.com/id/Custom-Macro-Mechanical-Keypad/
+
+// Edited for Macro Keypad by Preston Crowe 
+// Github Repository: https://github.com/PrestonCrowe/Macro-Keypad/
+
+//Editor Info
+// Edited February 2019
+// Github: PrestonCrowe
+// Instagram: @deeznutty2004
+
+
+//Original Creator Info
+// March-April 2018
+// Created for Instuctables, username: FosterP
+// Instagram: Lego_Rocket_08
+
+#include <Mouse.h>
+#include <Keyboard.h>
+
+// Defining each pin, with a unique name
+// S# = Switch #
+#define S1 4
+#define S2 5
+#define S3 6
+#define S4 7
+#define S5 8
+#define S6 9
+
+void setup() {
+  // Telling the Arduino that the switches are inputs, and the Leds are output //
+  pinMode (S1, INPUT_PULLUP);
+  pinMode (S2, INPUT_PULLUP);
+  pinMode (S3, INPUT_PULLUP);
+  pinMode (S4, INPUT_PULLUP);
+  pinMode (S5, INPUT_PULLUP);
+  pinMode (S6, INPUT_PULLUP);
+
+  Keyboard.begin();
+  Mouse.begin();
+}
+
+void loop() {
+
+    //Switch 2//
+    if (digitalRead(S2) == LOW)
+    {
+      Keyboard.press(KEY_UP_ARROW);
+    }
+    if (digitalRead(S2) == HIGH)
+    {
+      Keyboard.release(KEY_UP_ARROW);
+    }
+
+    //Switch 5//
+    if (digitalRead(S5) == LOW)
+    {
+      Keyboard.press(KEY_DOWN_ARROW);
+    }
+    if (digitalRead(S5) == HIGH)
+    {
+      Keyboard.release(KEY_DOWN_ARROW);
+    }
+
+    //Switch 6//
+    if (digitalRead(S6) == LOW)
+    {
+      Keyboard.press(KEY_RIGHT_ARROW);
+    }
+    if (digitalRead(S6) == HIGH)
+    {
+      Keyboard.release(KEY_RIGHT_ARROW);
+    }
+
+    //Switch 4//
+    if (digitalRead(S4) == LOW)
+    {
+      Keyboard.press(KEY_LEFT_ARROW);
+    }
+    if (digitalRead(S4) == HIGH)
+    {
+      Keyboard.release(KEY_LEFT_ARROW);
+    }
+
+    //Switch 3//
+    if (digitalRead(S3) == LOW)
+    {
+      Keyboard.press(KEY_LEFT_CTRL);
+    }
+    if (digitalRead(S3)==HIGH)
+    {
+      Keyboard.release(KEY_LEFT_CTRL);
+    }
+
+    //Switch 1//
+    if (digitalRead(S1) == LOW)
+    {
+      Keyboard.press(KEY_LEFT_ALT);
+    }
+    if (digitalRead(S1) == HIGH)
+    {
+      Keyboard.release(KEY_LEFT_ALT);
+    }
+  }
+}
